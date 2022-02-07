509c68e8b7024e7df8015feb174400ed37fb3692
Mateus Silva <mateuss_gt@hotmail.com>
2022-02-07 14:22:45 -0300

    configuração do docker

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:44384/api/",
+  baseURL: "https://db-locadora:3306/api/",
   useDefaultAuth: true,
   prettyJson: true,
   enableCors: true,
